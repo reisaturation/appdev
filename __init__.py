215e72b9c61675e3dc957fb73cd2059175e169bd
Home <202503E@mymail.nyp.edu.sg>
2021-01-06 18:04:46 +0800

    .

--- __init__.py
+++ __init__.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route('/selection')
-def selection():
-    return render_template("selection.html")
+@app.route('/aston')
+def aston():
+    return render_template("aston.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
